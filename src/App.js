b844a0e9ed6ff4935621cdeddc18213c97f95ffe
Josip Frljić <64381086+josipfrljic94@users.noreply.github.com>
2020-09-14 14:37:12 +0200

    Revert "redux filters"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import React from 'react';
-
+import data from './data.json'
 import Products from './components/Products';
 import Filter  from './components/Filter';
 import Cart  from './components/Cart';
-import store from "./store"
-import {Provider} from "react-redux";
-
 class App extends React.Component {
 constructor(){
   super()
   this.state={
-    
-   
+    products:data.products,
+    size:"",
+    sort:"",
+    type:"",
     cartProducts:localStorage.getItem("cartProducts") ? JSON.parse(localStorage.getItem("cartProducts")): []
   };
   
 }
+filtertypes(event){
+  if(event.target.value===""){
+    this.setState({type:event.target.value,products:data.products})
+    
+  }else{
+    this.setState({ type:event.target.value,  
+       products: data.products.filter(product=>product.type===event.target.value)}
+    )
+ 
+    }
+}
+sizefilter=(event)=>{
+  if(event.target.value===""){
+    this.setState({size:event.target.value, products:data.products})
+  }
+  else{
+    this.setState({size:event.target.value,
+      products:data.products.filter(product=>product.size.includes(event.target.value)===true)})
 
+  }
+}
+
+sortprice=(event)=>{
+ let sort= event.target.value
+this.setState((state)=>({
+  sort: sort,
+  products: state.products.slice().sort((a,b)=>{
+    return(
+      sort==="lowest" 
+    ? a.price >b.price
+    ? 1
+    :-1
+    : sort==='highest'
+    ? a.price < b.price
+    ? 1
+    :-1
+    : a.id > b.id
+    ? 1
+    : -1
+    // 
+  )})
+}))
+  }
 
 
   // CART  FUNCTION
@@ -48,7 +89,6 @@ localStorage.setItem("cartProducts",JSON.stringify(cartProducts.filter(item=>ite
 
 render(){
   return (
-    <Provider store={store}>
     <div className="App">
     <header  >
       <a href='/cart'> Shopping cart</a>
@@ -56,17 +96,16 @@ render(){
     </header>
     <div className='container'>
       <section className='filter-section'>
-      <Filter  />
+      <Filter count={this.state.products.length} type={this.state.type} filtertypes={this.filtertypes.bind(this)} sizefilter={this.sizefilter} size={this.state.size} sortprice={this.sortprice} sort={this.state.sort}/>
       <Cart cartProducts={this.state.cartProducts} removeProduct={this.removeProduct}/>
       </section>
      
 
-     <Products  addtocart={this.addtocart}/>
+     <Products products={this.state.products} addtocart={this.addtocart}/>
     
        
       </div>
     </div>
-    </Provider>
   )
 
 }
